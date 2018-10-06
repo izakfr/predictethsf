b55ed90331ec216c8005bb792d36318f0be7b3ad
Amiya Diwan <amiyadiwan@Amiyas-MacBook-Pro.local>
2018-10-05 23:38:40 -0700

    Add Jinja template base for landing page

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import boto3
 app = Flask(__name__)
+
+TEAMS_TABLE_NAME = 'dev_esf_team'
 
 
 @app.route('/')
 def hello():
-    return "Hello EthSF!"
+    return 'hello ethSF'
+
+@app.route('/teams')
+def landing():
+    db = boto3.resource('dynamodb')
+    teams_table = db.Table(TEAMS_TABLE_NAME)
+    data = teams_table.scan()['Items']
+    context = {}
+    context['my_string'] = 'StableSquad'
+    context['my_list'] = data
+    return render_template('landing.html', **context)
+
+@app.route('/instructions')
+def instructions():
+    return 'instructions'
 
