8fbd428f44534596fb09176c51a64196a1771e06
David Zhang <dazheng@dazheng-MacBook.local>
2018-10-07 03:05:51 -0700

    forever

--- infura-listener/run_mainnet.py
+++ infura-listener/run_mainnet.py
@@ -2,9 +2,9 @@ from listener import InfuraClient
 
 if __name__ == "__main__":
     client = InfuraClient(network='prod')
-    client.run()
-    # while True:
-    #     try:
-    #         client.run()
-    #     except:
-    #         pass
+    # client.run()
+    while True:
+        try:
+            client.run()
+        except:
+            pass
