80f1feb3a6f90ab69651142144394a97491c665b
Mahraan Qadir <mahraan@gmail.com>
2018-10-06 17:45:14 -0700

    front-end tweaks

--- flask-server/server.py
+++ flask-server/server.py
@@ -13,7 +13,24 @@ def hello():
 def index():
     results = TeamModel.scan()
     dict_results = sorted([dict(x) for x in results], key=lambda x: float(x['total_staked_ether']), reverse=True)
+
+    global_staked_ether = 0.0
+
+    for team in dict_results:
+    	global_staked_ether += float(team['total_staked_ether'])
+
+    for team in dict_results:
+    	team['implied_win'] = round((float(team['total_staked_ether']) / global_staked_ether) * 100, 2)
+    	team['total_staked_ether'] = round(float(team['total_staked_ether']), 3)
+    	if team['picture_url'] == None:
+    		team['picture_url'] = 'https://i.imgur.com/v3TqVnb.jpg'
+    	if team['project_link'] == None:
+    		team['project_link'] = ''
+    	if team['description'] == None:
+    		team['description'] = ''
+
     context = {}
+
     context['my_string'] = 'My first string'
     context['teams_list'] = dict_results
     return render_template('index.html', **context)
