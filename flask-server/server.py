a9a56b401083c47b3709f004d97f7ae5c1b06194
David Zhang <dazheng@dazheng-MacBook.local>
2018-10-07 00:13:02 -0700

    a;lsdkf

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request, redirect
 from teams.TeamModel import TeamModel
 from utils import DEV_PROD_VALUE
+import requests
+import urllib
 
 app = Flask(__name__)
 
+def format_url(url):
+    p = urllib.parse.urlparse(url, 'http')
+    netloc = p.netloc or p.path
+    path = p.path if p.netloc else ''
+    if not netloc.startswith('www.'):
+        netloc = 'www.' + netloc
+
+    p = urllib.parse.ParseResult('http', netloc, path, *p[3:])
+    return p.geturl()
 
 @app.route('/test')
 def hello():
@@ -11,26 +22,28 @@ def hello():
 
 @app.route('/')
 def index():
-    results = TeamModel.scan()
+    results = TeamModel.scan(TeamModel.mined == True)
     dict_results = sorted([dict(x) for x in results], key=lambda x: float(x['total_staked_ether']), reverse=True)
 
     global_staked_ether = 0.0
 
     for team in dict_results:
-    	global_staked_ether += float(team['total_staked_ether'])
+        global_staked_ether += float(team['total_staked_ether'])
 
     for team in dict_results:
-    	if global_staked_ether == 0:
-    		team['implied_win'] = 0
-    	else:
-    		team['implied_win'] = round((float(team['total_staked_ether']) / global_staked_ether) * 100, 2)
-    	team['total_staked_ether'] = round(float(team['total_staked_ether']), 3)
-    	if team['picture_url'] == None:
-    		team['picture_url'] = 'https://i.imgur.com/v3TqVnb.jpg'
-    	if team['project_link'] == None:
-    		team['project_link'] = ''
-    	if team['description'] == None:
-    		team['description'] = ''
+        if global_staked_ether == 0:
+            team['implied_win'] = 0
+        else:
+            team['implied_win'] = round((float(team['total_staked_ether']) / global_staked_ether) * 100, 2)
+        team['total_staked_ether'] = round(float(team['total_staked_ether']), 3)
+        if team['picture_url'] == None:
+            team['picture_url'] = 'https://i.imgur.com/v3TqVnb.jpg'
+        if team['project_link'] == None:
+            team['project_link'] = ''
+        if team['description'] == None:
+            team['description'] = ''
+        team['description'] = team['description'][:140]
+        team['project_link'] = format_url(team['project_link'])
 
     context = {}
 
